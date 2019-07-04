0bb0252addddb70374c0dc25a1fb8e9badaca741
Zhang Lei <942471854@qq.com>
2019-07-05 01:01:15 +0800

    commit-The problem of unable to read model has been solved. It is a problem of random number seed. It needs to be compiled for prediction function.-2019-7-5 01:01:08

--- practical_training/machine_learning/comments_machine_learning2.py
+++ practical_training/machine_learning/comments_machine_learning2.py
@@ -30,7 +30,7 @@ def snow_test(data):
 	print("准确率: " + str(counts / len(data)))
 
 
-def machine_learning1(data):
+def machine_learning(data):
 	def chinese_word_cut(text):
 		return " ".join(jieba.cut(text))
 
@@ -39,49 +39,8 @@ def machine_learning1(data):
 	X = data["cut_comment"]
 	y = data["evaluation"]
 
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-
-	def get_custom_stopwords(stop_words_file):
-		with open(stop_words_file, encoding="utf-8") as f:
-			stopwords = f.read()
-
-		stopwords_list = stopwords.split("\n")
-		custom_stopwords_list = [i for i in stopwords_list]
-		return custom_stopwords_list
-
-	stop_words_file = "./stop_words/哈工大停用词表.txt"
-	stopwords = get_custom_stopwords(stop_words_file)
-
-	vect = CountVectorizer(max_df=0.8, min_df=3, token_pattern=u'(?u)\\b[^\\d\\W]\\w+\\b',
-						   stop_words=frozenset(stopwords))
-
-	test = pd.DataFrame(vect.fit_transform(X_train).toarray(), columns=vect.get_feature_names())
-
-	nb = MultinomialNB()
-	X_train_vect = vect.fit_transform(X_train)
-	nb.fit(X_train_vect, y_train)
-	train_score = nb.score(X_train_vect, y_train)
-	print(train_score)
-
-	X_test_vect = vect.transform(X_test)
-	print(nb.score(X_test_vect, y_test))
-
-	X_vec = vect.transform(X)
-	nb_result = nb.predict(X_vec)
-	data['nb_result'] = nb_result
-
-	print(data)
-
-def machine_learning2(data):
-	def chinese_word_cut(text):
-		return " ".join(jieba.cut(text))
-
-	data["cut_comment"] = data.comment.apply(chinese_word_cut)
-
-	X = data["cut_comment"]
-	y = data["evaluation"]
-
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+	random_state = 42
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=random_state)
 
 	def get_custom_stopwords(stop_words_file):
 		with open(stop_words_file, encoding="utf-8") as f:
@@ -119,16 +78,15 @@ def machine_learning2(data):
 	y_test = to_categorical(y_test.map(lambda x: le.transform([x])[0]), nb_classes=len(unique_y_labels))
 
 	# 构造网络
-	n_epoch = 1000
+	n_epoch = 10
 	size_of_each_vector = X_train_padded_seqs.shape[1]
 	vocab_size = len(vocab)
 	no_of_unique_y_labels = len(unique_y_labels)
 
-	net = tflearn.input_data(
-		[None, size_of_each_vector])  # The first element is the "batch size" which we set to "None"
-	net = tflearn.embedding(net, input_dim=vocab_size, output_dim=128)  # input_dim: vocabulary size
-	net = tflearn.lstm(net, 128, dropout=0.6)  # Set the dropout to 0.6
-	net = tflearn.fully_connected(net, no_of_unique_y_labels, activation='softmax')  # relu or softmax
+	net = tflearn.input_data([None, size_of_each_vector])
+	net = tflearn.embedding(net, input_dim=vocab_size, output_dim=128)
+	net = tflearn.lstm(net, 128, dropout=0.6)
+	net = tflearn.fully_connected(net, no_of_unique_y_labels, activation='softmax')
 	net = tflearn.regression(net, optimizer='adam', learning_rate=1e-4, loss='categorical_crossentropy')
 
 	# 训练网络
@@ -143,15 +101,15 @@ def machine_learning2(data):
 	# 保存
 	time = datetime.now()
 	time_str = str(time).replace(":", ".")
-	os.makedirs(f"./tflearn_data/tflearn_modes/{time_str}")
-	model.save(f"./tflearn_data/tflearn_modes/{time_str}/comment_mode.tflearn")
+	os.makedirs(f"./tflearn_data/tflearn_models/{time_str}({n_epoch}, {random_state})")
+	model.save(f"./tflearn_data/tflearn_models/{time_str}({n_epoch}, {random_state})/model")
+
 
 if __name__ == '__main__':
 	comments_data = pd.read_csv("comments.csv")
 	# snow_test(comments_data)
 
 	start_time = datetime.now()
-	# machine_learning1(comments_data)
-	machine_learning2(comments_data)
+	machine_learning(comments_data)
 	end_time = datetime.now()
 	print("total time: " + str(end_time - start_time))
