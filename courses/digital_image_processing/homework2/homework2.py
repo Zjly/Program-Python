510b6b2767a69f754f6bce3b14db129f6b3aed0d
Zhang Lei <942471854@qq.com>
2019-10-09 20:55:49 +0800

    commit-完成数字图像处理homework2

--- courses/digital_image_processing/homework2/homework2.py
+++ courses/digital_image_processing/homework2/homework2.py
@@ -2,53 +2,95 @@ import cv2
 
 from courses.digital_image_processing.homework2.adding_noise import gauss_noise, uniformly_distributed_noise, \
 	salt_and_pepper_noise
-from courses.digital_image_processing.homework2.wave_filtering import arithmetic_mean_filter, geometric_mean_filter
+from courses.digital_image_processing.homework2.wave_filtering import arithmetic_mean_filter, geometric_mean_filter, \
+	harmonic_mean_filter, inverse_harmonic_mean_filter, maximum_filter, minimum_filter, median_filter, \
+	adaptive_median_filter
 
-if __name__ == '__main__':
+
+def add_noise():
+	"""
+	向图像添加噪声
+	:return:
+	"""
+	image_list = []
+
 	# 原始图像
 	original_image = cv2.imread("./image.png", cv2.IMREAD_GRAYSCALE)
 
 	# 高斯噪声图像: μ = 0, σ = 20
 	gauss_image = gauss_noise(original_image, 0, 20)
-	cv2.imwrite("./result/noise_image/gauss_image.png", gauss_image)
+	cv2.imwrite("./noise_image/gauss_image.png", gauss_image)
+	image_list.append(["gauss_image", gauss_image])
 
 	# 均匀分布噪声图像: a = -20, b = 20
 	uniform_image = uniformly_distributed_noise(original_image, -20, 20)
-	cv2.imwrite("./result/noise_image/uniform_image.png", uniform_image)
-
-	# 椒盐噪声图像:p(a) = 0.1, p(b) = 0.1
-	salt_and_pepper_image = salt_and_pepper_noise(original_image, 0.1, 0.1)
-	cv2.imwrite("./result/noise_image/salt_and_pepper_image.png", salt_and_pepper_image)
-
-	# 盐噪声图像:p(a) = 0, p(b) = 0.1
-	salt_image = salt_and_pepper_noise(original_image, 0, 0.1)
-	cv2.imwrite("./result/noise_image/salt_image.png", salt_image)
+	cv2.imwrite("./noise_image/uniform_image.png", uniform_image)
+	image_list.append(["uniform_image", uniform_image])
 
 	# 椒噪声图像:p(a) = 0.1, p(b) = 0
 	pepper_image = salt_and_pepper_noise(original_image, 0.1, 0)
-	cv2.imwrite("./result/noise_image/pepper_image.png", pepper_image)
+	cv2.imwrite("./noise_image/pepper_image.png", pepper_image)
+	image_list.append(["pepper_image", pepper_image])
 
-	# 算数均值滤波器
-	# gauss_image_arithmetic_mean_filter = arithmetic_mean_filter(gauss_image)
-	# cv2.imwrite("./result/arithmetic_mean_filter/gauss_image_arithmetic_mean_filter.png",
-	# 			gauss_image_arithmetic_mean_filter)
-	# uniform_image_arithmetic_mean_filter = arithmetic_mean_filter(uniform_image)
-	# cv2.imwrite("./result/arithmetic_mean_filter/uniform_image_arithmetic_mean_filter.png",
-	# 			gauss_image_arithmetic_mean_filter)
-	# salt_and_pepper_image_arithmetic_mean_filter = arithmetic_mean_filter(salt_and_pepper_image)
-	# cv2.imwrite("./result/arithmetic_mean_filter/salt_and_pepper_image_arithmetic_mean_filter.png",
-	# 			salt_and_pepper_image_arithmetic_mean_filter)
-	# salt_image_arithmetic_mean_filter = arithmetic_mean_filter(salt_image)
-	# cv2.imwrite("./result/arithmetic_mean_filter/salt_image_arithmetic_mean_filter.png",
-	# 			salt_image_arithmetic_mean_filter)
-	# pepper_image_arithmetic_mean_filter = arithmetic_mean_filter(pepper_image)
-	# cv2.imwrite("./result/arithmetic_mean_filter/pepper_image_arithmetic_mean_filter.png",
-	# 			pepper_image_arithmetic_mean_filter)
+	# 盐噪声图像:p(a) = 0, p(b) = 0.1
+	salt_image = salt_and_pepper_noise(original_image, 0, 0.1)
+	cv2.imwrite("./noise_image/salt_image.png", salt_image)
+	image_list.append(["salt_image", salt_image])
 
-	# 几何均值滤波器
-	gauss_image_geometric_mean_filter = geometric_mean_filter(gauss_image)
+	# 椒盐噪声图像:p(a) = 0.1, p(b) = 0.1
+	salt_and_pepper_image_10 = salt_and_pepper_noise(original_image, 0.1, 0.1)
+	cv2.imwrite("./noise_image/salt_and_pepper_image_p(0.1).png", salt_and_pepper_image_10)
+	image_list.append(["salt_and_pepper_image_p(0.1)", salt_and_pepper_image_10])
 
-	cv2.namedWindow('demo', 0)
-	cv2.imshow("demo", gauss_image_geometric_mean_filter)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+	# 椒盐噪声图像:p(a) = 0.25, p(b) = 0.25
+	salt_and_pepper_image_25 = salt_and_pepper_noise(original_image, 0.25, 0.25)
+	cv2.imwrite("./noise_image/salt_and_pepper_image_p(0.25).png", salt_and_pepper_image_25)
+	image_list.append(["salt_and_pepper_image_p(0.25)", salt_and_pepper_image_25])
+
+	return image_list
+
+
+def wave_filter(image_list):
+	"""
+	滤波处理
+	:param image_list: 图片列表
+	:return:
+	"""
+	for images in image_list:
+		image_name = images[0]
+		image = images[1]
+
+		# 算数均值滤波器
+		cv2.imwrite(f"./result/arithmetic_mean_filter/{image_name}.png", arithmetic_mean_filter(image))
+
+		# 几何均值滤波器
+		cv2.imwrite(f"./result/geometric_mean_filter/{image_name}.png", geometric_mean_filter(image))
+
+		# 谐波均值滤波器
+		cv2.imwrite(f"./result/harmonic_mean_filter/{image_name}.png", harmonic_mean_filter(image))
+
+		# 逆谐波均值滤波器
+		cv2.imwrite(f"./result/inverse_harmonic_mean_filter/{image_name}_1.5.png",
+					inverse_harmonic_mean_filter(image, 1.5))
+		cv2.imwrite(f"./result/inverse_harmonic_mean_filter/{image_name}_-1.5.png",
+					inverse_harmonic_mean_filter(image, -1.5))
+
+		# 最大值滤波器
+		cv2.imwrite(f"./result/maximum_filter/{image_name}.png", maximum_filter(image))
+
+		# 最小值滤波器
+		cv2.imwrite(f"./result/minimum_filter/{image_name}.png", minimum_filter(image))
+
+		# 中值滤波器
+		cv2.imwrite(f"./result/median_filter/{image_name}.png", median_filter(image))
+
+		# 自适应中值滤波器
+		cv2.imwrite(f"./result/adaptive_median_filter/{image_name}.png", adaptive_median_filter(image))
+
+
+if __name__ == '__main__':
+	# 添加噪声
+	img_list = add_noise()
+
+	# 图像滤波
+	wave_filter(img_list)
--- courses/digital_image_processing/homework2/wave_filtering.py
+++ courses/digital_image_processing/homework2/wave_filtering.py
@@ -1,3 +1,21 @@
+def is_in_range(x, y, height, width):
+	"""
+	该像素是否在图像中
+	:param x: 像素X坐标
+	:param y: 像素Y坐标
+	:param height: 图像高度
+	:param width: 图像宽度
+	:return: 像素是否在图像中
+	"""
+	if x < 0 or x >= height:
+		return False
+
+	if y < 0 or y >= width:
+		return False
+
+	return True
+
+
 def arithmetic_mean_filter(image):
 	"""
 	算数均值滤波器
@@ -9,16 +27,19 @@ def arithmetic_mean_filter(image):
 	i_height = am_image.shape[0]
 	i_width = am_image.shape[1]
 
-	for i in range(1, i_height - 1):
-		for j in range(1, i_width - 1):
+	for i in range(i_height):
+		for j in range(i_width):
 			sum_pixel = 0
+			count_pixel = 0
 
 			# 对该像素周围像素求算数均值
 			for x in range(i - 1, i + 2):
 				for y in range(j - 1, j + 2):
-					sum_pixel += image[x, y]
+					if is_in_range(x, y, i_height, i_width):
+						sum_pixel += image[x, y]
+						count_pixel += 1
 
-			am_image[i, j] = int(sum_pixel / 9)
+			am_image[i, j] = int(sum_pixel / count_pixel)
 
 	return am_image
 
@@ -34,15 +55,245 @@ def geometric_mean_filter(image):
 	i_height = gm_image.shape[0]
 	i_width = gm_image.shape[1]
 
-	for i in range(1, i_height - 1):
-		for j in range(1, i_width - 1):
-			sum_pixel = 1
+	for i in range(i_height):
+		for j in range(i_width):
+			product_pixel = 1
+			count_pixel = 0
 
 			# 对该像素周围像素求几何均值
 			for x in range(i - 1, i + 2):
 				for y in range(j - 1, j + 2):
-					sum_pixel *= image[x, y]
+					if is_in_range(x, y, i_height, i_width):
+						# 如若该点像素值为0则积*1，避免乘积直接变为0
+						if int(image[x, y]) == 0:
+							product_pixel *= 1
+						else:
+							product_pixel *= int(image[x, y])
+						count_pixel += 1
 
-			gm_image[i, j] = int(pow(sum_pixel, 1 / 9))
+			gm_image[i, j] = int(pow(product_pixel, 1 / count_pixel))
 
 	return gm_image
+
+
+def harmonic_mean_filter(image):
+	"""
+	谐波均值滤波器
+	:param image: 原始图像
+	:return: 谐波均值滤波之后的图像
+	"""
+	hm_image = image.copy()
+
+	i_height = hm_image.shape[0]
+	i_width = hm_image.shape[1]
+
+	for i in range(i_height):
+		for j in range(i_width):
+			sum_pixel = 0
+			count_pixel = 0
+
+			# 进行谐波均值滤波计算
+			for x in range(i - 1, i + 2):
+				for y in range(j - 1, j + 2):
+					if is_in_range(x, y, i_height, i_width):
+						# 如若该点像素值为0则和+1，避免除零错误
+						if image[x, y] == 0:
+							sum_pixel += 1
+						else:
+							sum_pixel += 1 / image[x, y]
+
+						count_pixel += 1
+
+			image_pixel = int(count_pixel / sum_pixel)
+
+			# 将像素值保持在[0, 255]之间
+			if 0 <= image_pixel <= 255:
+				hm_image[i, j] = image_pixel
+			elif image_pixel < 0:
+				hm_image[i, j] = 0
+			elif image_pixel > 255:
+				hm_image[i, j] = 255
+
+	return hm_image
+
+
+def inverse_harmonic_mean_filter(image, Q):
+	"""
+	逆谐波均值滤波器
+	:param Q: 阶数
+	:param image: 原始图像
+	:return: 逆谐波均值滤波之后的图像
+	"""
+	ihm_image = image.copy()
+
+	i_height = ihm_image.shape[0]
+	i_width = ihm_image.shape[1]
+
+	for i in range(i_height):
+		for j in range(i_width):
+			molecule = 0
+			denominator = 0
+
+			# 进行逆谐波均值滤波计算
+			for x in range(i - 1, i + 2):
+				for y in range(j - 1, j + 2):
+					if is_in_range(x, y, i_height, i_width):
+						pixel = int(image[x, y])
+
+						# 设置像素为0的点为1，避免0的负指数的错误
+						if pixel == 0:
+							pixel = 1
+
+						molecule += pow(pixel, Q + 1)
+						denominator += pow(pixel, Q)
+
+			ihm_image[i, j] = int(molecule / denominator)
+
+	return ihm_image
+
+
+def maximum_filter(image):
+	"""
+	最大值滤波器
+	:param image: 原始图像
+	:return: 最大值滤波后的图像
+	"""
+	m_image = image.copy()
+
+	i_height = m_image.shape[0]
+	i_width = m_image.shape[1]
+
+	for i in range(i_height):
+		for j in range(i_width):
+			max_pixel = 0
+
+			# 进行最大值滤波计算
+			for x in range(i - 1, i + 2):
+				for y in range(j - 1, j + 2):
+					if is_in_range(x, y, i_height, i_width):
+						if image[x, y] > max_pixel:
+							max_pixel = image[x, y]
+
+			m_image[i, j] = max_pixel
+
+	return m_image
+
+
+def minimum_filter(image):
+	"""
+	最小值滤波器
+	:param image: 原始图像
+	:return: 最小值滤波后的图像
+	"""
+	m_image = image.copy()
+
+	i_height = m_image.shape[0]
+	i_width = m_image.shape[1]
+
+	for i in range(i_height):
+		for j in range(i_width):
+			min_pixel = 255
+
+			# 进行最小值滤波计算
+			for x in range(i - 1, i + 2):
+				for y in range(j - 1, j + 2):
+					if is_in_range(x, y, i_height, i_width):
+						if image[x, y] < min_pixel:
+							min_pixel = image[x, y]
+
+			m_image[i, j] = min_pixel
+
+	return m_image
+
+
+def median_filter(image):
+	"""
+	中值滤波器
+	:param image: 原始图像
+	:return: 中值滤波之后的图像
+	"""
+	m_image = image.copy()
+
+	i_height = m_image.shape[0]
+	i_width = m_image.shape[1]
+
+	for i in range(i_height):
+		for j in range(i_width):
+			array = []
+
+			# 进行中值滤波计算
+			for x in range(i - 1, i + 2):
+				for y in range(j - 1, j + 2):
+					if is_in_range(x, y, i_height, i_width):
+						array.append(image[x, y])
+
+			# 对周围像素进行排序
+			array.sort()
+
+			# 取中值作为该像素的值
+			m_image[i, j] = array[int((len(array) - 1) / 2)]
+
+	return m_image
+
+
+def adaptive_median_filter(image):
+	"""
+	自适应中值滤波器
+	:param image: 原始图像
+	:return: 自适应中值滤波后的图像
+	"""
+	am_image = image.copy()
+
+	i_height = am_image.shape[0]
+	i_width = am_image.shape[1]
+
+	for i in range(i_height):
+		for j in range(i_width):
+			# 当前模板尺寸，直至最大尺寸为止
+			size = 3
+			max_size = 7
+
+			while True:
+				# 放大边界数
+				delta = int((size - 1) / 2)
+
+				# 像素数组
+				array = []
+				# 将像素置入像素数组
+				for x in range(i - delta, i + delta + 1):
+					for y in range(j - delta, j + delta + 1):
+						if is_in_range(x, y, i_height, i_width):
+							array.append(image[x, y])
+
+				array.sort()
+
+				z_min = int(array[0])
+				z_max = int(array[len(array) - 1])
+				z_med = int(array[int(len(array) / 2)])
+				z_xy = int(image[i, j])
+
+				# A层次，判断z_med是否为脉冲
+				a1 = z_med - z_min
+				a2 = z_med - z_max
+
+				if not (a1 > 0 and a2 < 0):
+					# 模板尺寸还可以放大
+					if size < max_size:
+						size += 2
+						continue
+					else:
+						am_image[i, j] = z_med
+						break
+
+				# B层次，判断z_xy本身是否为脉冲
+				b1 = z_xy - z_min
+				b2 = z_xy - z_max
+
+				if b1 > 0 and b2 < 0:
+					am_image[i, j] = z_xy
+					break
+				else:
+					am_image[i, j] = z_med
+					break
+
+	return am_image
