7bd87bea20b764036b66192ef2977578bee8bc84
Zhang Lei <942471854@qq.com>
2020-05-08 12:07:49 +0800

    commit-信息安全算法改进

--- courses/computer_security/elliptic_curve.py
+++ courses/computer_security/elliptic_curve.py
@@ -2,33 +2,33 @@ from courses.computer_security.multiplicative_inverse import mul_inv
 
 
 def same():
-	x = 7
-	y = 9
-	mod = 11
+	x = 3
+	y = 2
+	mod = 19
 
 	# 计算分母分子
-	up = (3 * x ** 2 + 1) % mod
+	up = (3 * x ** 2 + 8) % mod
 	down = (2 * y) % mod
 
 	num_lambda = (up * mul_inv(down, mod)) % mod
-	x3 = (num_lambda ** 2 - x - x) % 11
-	y3 = ((x - x3) * num_lambda - y) % 11
+	x3 = (num_lambda ** 2 - x - x) % mod
+	y3 = ((x - x3) * num_lambda - y) % mod
 	print("x3 =", x3, ", y3 =", y3)
 
 def not_same():
-	x1 = 7
-	y1 = 9
-	x2 = 2
-	y2 = 4
-	mod = 11
+	x1 = 10
+	y1 = 8
+	x2 = 3
+	y2 = 2
+	mod = 19
 
 	# 计算分母分子
 	up = (y2 - y1) % mod
 	down = (x2 - x1) % mod
 
 	num_lambda = (up * mul_inv(down, mod)) % mod
-	x3 = (num_lambda ** 2 - x1 - x2) % 11
-	y3 = ((x1 - x3) * num_lambda - y1) % 11
+	x3 = (num_lambda ** 2 - x1 - x2) % mod
+	y3 = ((x1 - x3) * num_lambda - y1) % mod
 	print("x3 =", x3, ", y3 =", y3)
 
 if __name__ == '__main__':
